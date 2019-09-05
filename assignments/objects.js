45994048016da9ed742e4d34db1328e61a49b8f2
HKinslow <heatherkinslow6@gmail.com>
2019-09-04 22:39:07 -0400

    worked on objects and arrays

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,28 +18,72 @@ const example = {
 }
 
 // Write your intern objects here:
+const example1 = {
+  id: 1,
+  name: 'Mitzi',
+  email: 'mmelloy0@psu.edu',
+  gender: 'F',
+}
 
+const example2 = {
+  id: 2,
+  name: 'Kennan',
+  email: 'kdiben1@tinypic.com',
+  gender: 'M',
+  speak: function() {
+    return "Hi, my name is " + this.name;
+  }
+} 
+
+const example3 = {
+  id: 3,
+  name: 'Keven',
+  email: 'kmummery2@wikimedia.org',
+  gender: 'M',
+}
+
+const example4 = {
+  id: 4,
+  name: 'Gannie',
+  email: 'gmartinson3@illinois.edu',
+  gender: 'M',
+}
+
+const example5 = {
+  id: 5,
+  name: 'Antonietta',
+  email: 'adaine5@samsung.com',
+  gender: 'F',
+  multiplyNums: function(num3, num4) {
+    return num3 * num4;
+  }
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+ console.log(example1, 'name');
 
 // Kennan's ID
+ console.log(example2, 'id');
 
 // Keven's email
+ console.log(example3, 'email');
 
 // Gannie's name
+ console.log(example4, 'name');
 
 // Antonietta's Gender
+ console.log(example5, 'gender');
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
-
+console.log(example2.speak())
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-
+console.log(example5.multiplyNums(3,4));
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
